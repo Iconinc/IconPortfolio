79b54d08c434819ec47226d6b910096ac3e1743a
Creator <creator@Igors-MacBook-Pro.local>
2025-02-25 18:03:31 +0200

    adding recent UI Posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         <section id={'Products'}>
             <Projects/>
         </section>
+        <Blogs/>
         <Bento/>
         <Testimonials/>
     </section>
--- src/components/sections/home/blogs.tsx
+++ src/components/sections/home/blogs.tsx
@@ -1,91 +1,107 @@
 "use client";
-import {Card, Carousel } from "@/components/ui/blog-carousel";
+import {Card, Carousel} from "@/components/ui/blog-carousel";
 import Image from "next/image";
 import React from "react";
 
 export function Blogs() {
     const cards = data.map((card, index) => (
-        <Card key={card.src} card={card} index={index} layout={true} />
+        <Card key={card.src} card={card} index={index} layout={true}/>
     ));
 
     return (
         <div className="w-full h-full py-20 bg-neutral-100 dark:bg-neutral-950">
             <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
-                Get to know your iSad.
+                Recent UI/UX
             </h2>
-            <Carousel items={cards} />
+            <Carousel items={cards}/>
         </div>
     );
 }
 
-const DummyContent = () => {
+interface DummyContentProps {
+    figma?: string,
+    prototype?: string
+}
+
+const DummyContent = ({figma, prototype}: DummyContentProps) => {
     return (
-        <>
-            {[...new Array(3).fill(1)].map((_, index) => {
-                return (
-                    <div
-                        key={"dummy-content" + index}
-                        className="bg-[#F5F5F7] p-8 md:p-14 rounded-3xl mb-4"
-                    >
-                        <p className="text-neutral-600 text-base md:text-2xl font-sans max-w-3xl mx-auto">
-              <span className="font-bold text-neutral-700">
-                The first rule of Apple club is that you boast about Apple club.
-              </span>{" "}
-                            Keep a journal, quickly jot down a grocery list, and take amazing
-                            class notes. Want to convert those notes to text? No problem.
-                            Langotiya jeetu ka mara hua yaar is ready to capture every
-                            thought.
-                        </p>
-                        <Image
-                            src="https://assets.aceternity.com/macbook.png"
-                            alt="Macbook mockup from Aceternity UI"
-                            height="500"
-                            width="500"
-                            className="md:w-1/2 md:h-1/2 h-full w-full mx-auto object-contain"
-                        />
-                    </div>
-                );
-            })}
-        </>
+        <div
+            key={"dummy-content"}
+            className="rounded-3xl mb-4 flex-center flex-col gap-2 p-2"
+        >
+            <iframe className={'border border-[rgba(0, 0, 0, 0.1)] rounded-2xl w-full'}
+                    width={800} height={550}
+                    src={figma}
+                    allowFullScreen></iframe>
+
+            <iframe className={'border border-[rgba(0, 0, 0, 0.1)] rounded-2xl w-full'}
+                    width={800} height={550}
+                    src={prototype}
+                    allowFullScreen></iframe>
+        </div>
     );
 };
 
 const data = [
     {
-        category: "Artificial Intelligence",
-        title: "You can do more with AI.",
-        src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        content: <DummyContent />,
+        category: "Education",
+        title: "You learn more with TooNation",
+        src: "/assets/too.jpg",
+        content: <DummyContent
+            figma={'https://embed.figma.com/design/O2ISTHLzYrPkpu76Z3rwwM/Too-Nation?node-id=0-1&embed-host=share'}
+            prototype={'https://embed.figma.com/proto/O2ISTHLzYrPkpu76Z3rwwM/Too-Nation?page-id=0%3A1&node-id=1-2&embed-host=share'}/>,
     },
     {
-        category: "Productivity",
-        title: "Enhance your productivity.",
-        src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        content: <DummyContent />,
+        category: "Health",
+        title: "Enhance female menstruation with Tantine.",
+        src: "/assets/tantine.jpg",
+        content: <DummyContent figma={'https://embed.figma.com/design/JGIC4cyjI1okmwN3ST3ye9/Tantine-app?node-id=1-4&embed-host=share'}
+                               prototype={'https://embed.figma.com/proto/JGIC4cyjI1okmwN3ST3ye9/Tantine-app?node-id=485-2177&scaling=scale-down&content-scaling=fixed&page-id=1%3A4&starting-point-node-id=485%3A2185&embed-host=share'}/>,
     },
     {
-        category: "Product",
-        title: "Launching the new Apple Vision Pro.",
-        src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        content: <DummyContent />,
-    },
-
-    {
-        category: "Product",
-        title: "Maps for your iPhone 15 Pro Max.",
-        src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        content: <DummyContent />,
+        category: "Creative",
+        title: "Exprole Trands with Lennox",
+        src: "/assets/lennox.jpg",
+        content: <DummyContent figma={'htps://embed.figma.com/design/CTw5vIPD4fN6OWDtrYl8c4/Lennox?node-id=6-45&embed-host=share'}
+                               prototype={'https://embed.figma.com/proto/CTw5vIPD4fN6OWDtrYl8c4/Lennox?node-id=8-88&scaling=scale-down&content-scaling=fixed&page-id=6%3A44&embed-host=share'}/>,
     },
     {
-        category: "iOS",
-        title: "Photography just got better.",
-        src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        content: <DummyContent />,
+        category: "Education",
+        title: "Univease",
+        src: "/assets/univease.jpg",
+        figma: 'https://embed.figma.com/design/r2ssM17YvHjoX0DIbO8F8z/UnivEase?node-id=8-44&embed-host=share',
+        prototype: 'https://embed.figma.com/proto/r2ssM17YvHjoX0DIbO8F8z/UnivEase?node-id=185-71&p=f&scaling=scale-down&content-scaling=fixed&page-id=8%3A44&starting-point-node-id=185%3A71&embed-host=share',
+        content: <DummyContent figma={'https://embed.figma.com/design/O2ISTHLzYrPkpu76Z3rwwM/Too-Nation?node-id=0-1&embed-host=share'}
+                               prototype={'https://embed.figma.com/proto/O2ISTHLzYrPkpu76Z3rwwM/Too-Nation?page-id=0%3A1&node-id=1-2&embed-host=share'}/>,
+    },
+    {
+        category: "Agency",
+        title: "Sekadi Software Agency",
+        src: "/assets/sekadi.jpg",
+        figma: 'https://embed.figma.com/design/EzdGN1IZAiiq0dIUL87X5h/Sekadi-refubrished?node-id=0-1&embed-host=share',
+        prototype: 'https://embed.figma.com/proto/QpXKE62f9U1ZjvFo6w7RGJ/OWC?page-id=3%3A3&node-id=115-898&p=f&viewport=-495%2C298%2C0.15&scaling=scale-down&content-scaling=fixed&starting-point-node-id=64%3A585&embed-host=share',
+        content: <DummyContent figma={'https://embed.figma.com/design/O2ISTHLzYrPkpu76Z3rwwM/Too-Nation?node-id=0-1&embed-host=share'}
+                               prototype={'https://embed.figma.com/proto/O2ISTHLzYrPkpu76Z3rwwM/Too-Nation?page-id=0%3A1&node-id=1-2&embed-host=share'}/>,
+    },
+    {
+        category: "Tourist",
+        title: "Timeline Travel & Tours",
+        src: "/assets/ttt.jpg",
+        content: <DummyContent  figma={'https://embed.figma.com/design/99IzCQjbkpP2B3hpJgX9o7/TimelineTravel%26Tours?node-id=0-1&embed-host=share'}
+                                prototype={'https://embed.figma.com/proto/99IzCQjbkpP2B3hpJgX9o7/TimelineTravel%26Tours?node-id=1-4&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A4&embed-host=share'}/>,
     },
     {
         category: "Hiring",
-        title: "Hiring for a Staff Software Engineer",
-        src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        content: <DummyContent />,
+        title: "Obsessed with Capital",
+        src: "/assets/owc.jpg",
+        content: <DummyContent  figma={'https://embed.figma.com/design/QpXKE62f9U1ZjvFo6w7RGJ/OWC?node-id=3-3&embed-host=share'}
+                                prototype= {'https://embed.figma.com/proto/QpXKE62f9U1ZjvFo6w7RGJ/OWC?page-id=3%3A3&node-id=64-585&p=f&viewport=-495%2C298%2C0.15&scaling=scale-down&content-scaling=fixed&starting-point-node-id=64%3A585&embed-host=share'}/>
     },
+    {
+        category: "Education",
+        title: "History Interaction Timeline",
+        src: "/assets/hit.jpg",
+        content: <DummyContent  figma={'https://embed.figma.com/design/tibbspW0iNcSqw49ebu5tx/History-Interaction?node-id=48-2&embed-host=share'}
+                                prototype={'https://embed.figma.com/proto/tibbspW0iNcSqw49ebu5tx/History-Interaction?node-id=106-31182&scaling=min-zoom&content-scaling=fixed&page-id=48%3A2&embed-host=share'}/>,
+    }
 ];
